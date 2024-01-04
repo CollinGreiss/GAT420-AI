f1da4b6ec5f7e719c424fd05d6ada3763e664b33
CollinGreiss <cgreiss@student.neumont.edu>
2024-01-04 14:19:12 -0700

    AI - Elementary Cellular Automaton

--- Src/A-Life/Environment.h
+++ Src/A-Life/Environment.h
@@ -6,14 +6,16 @@
 #include <memory>
 #include <vector>
 
-class Environment
-{
+class Environment {
+
 public:
-	Environment(int width, int height, std::shared_ptr<class Texture> texture) : 
-		size{ width, height },
-		texture{ texture } 
-	{
-		color_buffer.resize(width * height);
+
+	Environment( int width, int height, std::shared_ptr<class Texture> texture ) :
+
+		size { width, height },
+		texture { texture } {
+		color_buffer.resize( width * height );
+
 	}
 
 	virtual bool Initialize() = 0;
@@ -21,54 +23,57 @@ public:
 
 	// buffers
 	template<typename T>
-	T Read(const std::vector<T>& buffer, int x, int y, bool envWrap = true);
+	T Read( const std::vector<T>& buffer, int x, int y, bool envWrap = true );
 	template<typename T>
-	void Write(std::vector<T>& buffer, int x, int y, const T& value, bool envWrap = true);
+	void Write( std::vector<T>& buffer, int x, int y, const T& value, bool envWrap = true );
 
 	const std::vector<rgba_t>& GetColorBuffer() const { return color_buffer; }
 
 	// input
-	virtual void KeyDown(SDL_Keycode keycode);
-	virtual void MouseButtonDown(int mouse);
-	virtual void MouseButtonUp(int mouse);
-	virtual void MousePositionNormalized(const glm::vec2& position);
+	virtual void KeyDown( SDL_Keycode keycode );
+	virtual void MouseButtonDown( int mouse );
+	virtual void MouseButtonUp( int mouse );
+	virtual void MousePositionNormalized( const glm::vec2& position );
 
 protected:
-	glm::ivec2 size{ 0, 0 };
+
+	glm::ivec2 size { 0, 0 };
 
 	// input
-	glm::ivec2 mouse_position{ 0, 0 };
-	glm::ivec2 mouse_position_prev{ 0, 0 };
-	glm::ivec2 mouse_relative{ 0, 0 };
+	glm::ivec2 mouse_position { 0, 0 };
+	glm::ivec2 mouse_position_prev { 0, 0 };
+	glm::ivec2 mouse_relative { 0, 0 };
 	uint8_t mouse_button[3] = { 0, 0, 0 };
 
 	// color buffer -> texture
 	std::vector<rgba_t> color_buffer;
 	std::shared_ptr<class Texture> texture;
+
 };
 
 template<typename T>
-inline T Environment::Read(const std::vector<T>& buffer, int x, int y, bool wrapValue)
-{
-	if (wrapValue)
-	{
-		x = wrap(x, 0, size.x);
-		y = wrap(y, 0, size.y);
-	}
-	else if (x < 0 || x >= size.x || y < 0 || y >= size.y) return 0;
+inline T Environment::Read( const std::vector<T>& buffer, int x, int y, bool wrapValue ) {
 
-	return buffer[x + (y * size.x)];
+	if ( wrapValue ) {
+
+		x = wrap( x, 0, size.x );
+		y = wrap( y, 0, size.y );
+
+	} else if ( x < 0 || x >= size.x || y < 0 || y >= size.y ) return 0;
+
+	return buffer[x + ( y * size.x )];
+
 }
 
 template<typename T>
-inline void Environment::Write(std::vector<T>& buffer, int x, int y, const T& value, bool wrapValue)
-{
-	if (wrapValue)
-	{
-		x = wrap(x, 0, size.x);
-		y = wrap(y, 0, size.y);
-	}
-	else if (x < 0 || x >= size.x || y < 0 || y >= size.y) return;
+inline void Environment::Write( std::vector<T>& buffer, int x, int y, const T& value, bool wrapValue ) {
 
-	buffer[x + (y * size.x)] = value;
+	if ( wrapValue ) {
+		x = wrap( x, 0, size.x );
+		y = wrap( y, 0, size.y );
+
+	} else if ( x < 0 || x >= size.x || y < 0 || y >= size.y ) return;
+
+	buffer[x + ( y * size.x )] = value;
+
 }
